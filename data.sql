ec6360470d97514bfc6d996d99c9e9dea4a5285c
mukesh <mukeshranjan2007@gmail.com>
2020-04-12 04:05:21 -0400

    Second Commit

---
+++ data.sql
@@ -0,0 +1,6 @@
+insert into user values (123,sysdate,'mukesh');
+insert into user values (124,sysdate,'manushi');
+insert into user values (125,sysdate,'Shubha');
+insert into post values (1,'my first book', 123);
+insert into post values (2,'my first book', 123);
+insert into post values (3,'my first book', 125);
